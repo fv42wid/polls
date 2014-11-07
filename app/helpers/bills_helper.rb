2a4e1bedcf6fae774d9ee017d5c286e5a64ae718
Frank Vinci <fv42wid@yahoo.com>
2014-11-07 16:20:53 -0500

    disabled user poll form when user has voted or not logged in

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,2 +1,15 @@
 module BillsHelper
+
+  def user_voted?
+    if user_signed_in?
+      if UserPoll.find_by_user_id_and_bill_id(current_user.id, @bill.id)
+        return true
+      else
+        return false
+      end
+    else
+      return true
+    end
+  end
+
 end
